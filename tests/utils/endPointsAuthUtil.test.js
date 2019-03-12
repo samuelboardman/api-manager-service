491e5653606075ca890d5b0210feec8df97ea124
Samuel BoardMan <samelikzra@gmail.com>
2019-03-12 18:23:01 +0100

    initial commit

--- tests/utils/endPointsAuthUtil.test.js
+++ tests/utils/endPointsAuthUtil.test.js
@@ -3,6 +3,10 @@ const endPointsAuthUtil = require('../utils/endPointsAuthUtil')
 describe('Basic token', () => {
   it('should generate a basic token', () => {
     const result = endPointsAuthUtil.basicAuth({ username: 'Samuel', password: '123456B' })
-   expect(result).toBe('Basic U2FtdWVsOjEyMzQ1NkI=')
+    expect(result).toBe('Basic U2FtdWVsOjEyMzQ1NkI=')
+  })
+  it('should generate a basic token', () => {
+    const result = endPointsAuthUtil.basicAuth({ username: 'Samuel', password: '123456B' })
+   //g expect(result).('Basic Uii')
   })
 })
