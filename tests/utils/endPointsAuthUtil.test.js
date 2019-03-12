7521c37a20c2423fe807be0123469c3000b6c4d6
Samuel BoardMan <samelikzra@gmail.com>
2019-03-12 18:29:41 +0100

    fixed constant

--- tests/utils/endPointsAuthUtil.test.js
+++ tests/utils/endPointsAuthUtil.test.js
@@ -6,7 +6,7 @@ describe('Basic token', () => {
     expect(result).toBe('Basic U2FtdWVsOjEyMzQ1NkI=')
   })
   it('should generate a basic token', () => {
-    const result = endPointsAuthUtil.basicAuth({ username: 'Samuel', password: '123456B' })
-   //g expect(result).('Basic Uii')
+    // const result = endPointsAuthUtil.basicAuth({ username: 'Samuel', password: '123456B' })
+    // expect(result).('Basic Uii')
   })
 })
